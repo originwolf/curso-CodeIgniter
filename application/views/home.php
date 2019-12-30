de71363c50873e8a5d517ba332a29d53879ec010
Pedro Martins <pedrogmhead@gmail.com>
2019-12-30 17:02:03 -0300

    translated the site

--- application/views/home.php
+++ application/views/home.php
@@ -3,8 +3,8 @@
 				<div class="row">
 					<div class="col-lg-12 text-center">
 						<div class="section-title">
-							<h2>ABOUT</h2>
-							<p>A creative agency based on Candy Land, ready to boost your business with some beautifull templates. Lattes Agency is one of the best in town see more you will be amazed.</p>
+							<h2>Sobre</h2>
+							<p>Promovemos cursos de alta qualidade com profissionais graduados, com mestrado e doutorado.</p>
 						</div>
 					</div>
 				</div>
@@ -16,26 +16,26 @@
 				<div class="row">
 					<div class="col-md-3 mb-sm-30">
 						<div class="counter-item">
-							<h2 data-count="59">59</h2>
-							<h6>awards</h6>
+							<h2 data-count="59">X</h2>
+							<h6>instrutores</h6>
 						</div>
 					</div>
 					<div class="col-md-3 mb-sm-30">
 						<div class="counter-item">
-							<h2 data-count="1054">1054</h2>
-							<h6>Clients</h6>
+							<h2 data-count="1054">X</h2>
+							<h6>Alunos</h6>
 						</div>
 					</div>
 					<div class="col-md-3 mb-sm-30">
 						<div class="counter-item">
-							<h2 data-count="34">34</h2>
-							<h6>Team</h6>
+							<h2 data-count="34">X</h2>
+							<h6>Cursos</h6>
 						</div>
 					</div>
 					<div class="col-md-3 mb-sm-30">
 						<div class="counter-item">
-							<h2 data-count="154">154</h2>
-							<h6>Project</h6>
+							<h2 data-count="154">X</h2>
+							<h6>Avaliações</h6>
 						</div>
 					</div>
 				</div>
@@ -46,8 +46,8 @@
 			<div class="row">
 				<div class="col-lg-12 text-center">
 					<div class="section-title">
-						<h2>Portfolio</h2>
-						<p>Our portfolio is the best way to show our work, you can see here a big range of our work. Check them all and you will find what you are looking for.</p>
+						<h2>Cursos</h2>
+						<p>Conheça nossa lista de cursos.</p>
 					</div>
 				</div>
 			</div>
@@ -66,79 +66,6 @@
 					</div>
 				</div>
 				<!-- end portfolio item -->
-				<!-- start portfolio item -->
-				<div class="col-md-4">
-					<div class="ot-portfolio-item">
-						<figure class="effect-bubba">
-							<img src="<?php echo base_url(); ?>public/images/demo/portfolio-2.jpg" alt="img02" class="img-responsive" />
-							<figcaption>
-								<h2>Startup Framework</h2>
-								<p>Branding, Web Design</p>
-								<a href="#" data-toggle="modal" data-target="#Modal-2">View more</a>
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-				<!-- end portfolio item -->
-				<!-- start portfolio item -->
-				<div class="col-md-4">
-					<div class="ot-portfolio-item">
-						<figure class="effect-bubba">
-							<img src="<?php echo base_url(); ?>public/images/demo/portfolio-3.jpg" alt="img02" class="img-responsive" />
-							<figcaption>
-								<h2>Lamp & Velvet</h2>
-								<p>Branding, Web Design</p>
-								<a href="#" data-toggle="modal" data-target="#Modal-3">View more</a>
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-				<!-- end portfolio item -->
-			</div>
-			<div class="row">
-				<!-- start portfolio item -->
-				<div class="col-md-4">
-					<div class="ot-portfolio-item">
-						<figure class="effect-bubba">
-							<img src="<?php echo base_url(); ?>public/images/demo/portfolio-4.jpg" alt="img02" class="img-responsive" />
-							<figcaption>
-								<h2>Smart Name</h2>
-								<p>Branding, Design</p>
-								<a href="#" data-toggle="modal" data-target="#Modal-4">View more</a>
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-				<!-- end portfolio item -->
-				<!-- start portfolio item -->
-				<div class="col-md-4">
-					<div class="ot-portfolio-item">
-						<figure class="effect-bubba">
-							<img src="<?php echo base_url(); ?>public/images/demo/portfolio-5.jpg" alt="img02" class="img-responsive" />
-							<figcaption>
-								<h2>Fast People</h2>
-								<p>Branding, Web Design</p>
-								<a href="#" data-toggle="modal" data-target="#Modal-5">View more</a>
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-				<!-- end portfolio item -->
-				<!-- start portfolio item -->
-				<div class="col-md-4">
-					<div class="ot-portfolio-item">
-						<figure class="effect-bubba">
-							<img src="<?php echo base_url(); ?>public/images/demo/portfolio-2.jpg" alt="img02" class="img-responsive" />
-							<figcaption>
-								<h2>Kites & Stars</h2>
-								<p>Branding, Web Design</p>
-								<a href="#" data-toggle="modal" data-target="#Modal-2">View more</a>
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-				<!-- end portfolio item -->
-			</div>
 			</div><!-- end container -->
 		</section>
 		<section id="team" class="light-bg">
@@ -146,8 +73,8 @@
 				<div class="row">
 					<div class="col-lg-12 text-center">
 						<div class="section-title">
-							<h2>Our Team</h2>
-							<p>A creative agency based on Candy Land, ready to boost your business with some beautifull templates. Lattes Agency is one of the best in town see more you will be amazed.</p>
+							<h2>NOSSA EQUIPE</h2>
+							<p>Conheça nossa equipe.</p>
 						</div>
 					</div>
 				</div>
@@ -167,51 +94,6 @@
 						</div>
 					</div>
 					<!-- end team member item -->
-					<!-- team member item -->
-					<div class="col-md-3">
-						<div class="team-item">
-							<div class="team-image">
-								<img src="<?php echo base_url(); ?>public/images/demo/author-6.jpg" class="img-responsive" alt="author">
-							</div>
-							<div class="team-text">
-								<h3>LINA GOSATA</h3>
-								<div class="team-location">Los Angeles, California</div>
-								<div class="team-position">– Photography –</div>
-								<p>Worsa dona namet, cons uectetur dipiscing adon elit. Aliquam vitae fringilla unda mir.</p>
-							</div>
-						</div>
-					</div>
-					<!-- end team member item -->
-					<!-- team member item -->
-					<div class="col-md-3">
-						<div class="team-item">
-							<div class="team-image">
-								<img src="<?php echo base_url(); ?>public/images/demo/author-3.jpg" class="img-responsive" alt="author">
-							</div>
-							<div class="team-text">
-								<h3>Larry Parker</h3>
-								<div class="team-location">Barcelona, Spain</div>
-								<div class="team-position">– Development –</div>
-								<p>Aradan bes namet, cons uectetur moiscing adon elit. Aliquam vitae fringilla unda augue.</p>
-							</div>
-						</div>
-					</div>
-					<!-- end team member item -->
-					<!-- team member item -->
-					<div class="col-md-3">
-						<div class="team-item">
-							<div class="team-image">
-								<img src="<?php echo base_url(); ?>public/images/demo/author-4.jpg" class="img-responsive" alt="author">
-							</div>
-							<div class="team-text">
-								<h3>John BEKERS</h3>
-								<div class="team-location">Miami, Floria</div>
-								<div class="team-position">– Marketing –</div>
-								<p>Dolor sit don namet, cons uectetur beriscing adon elit. Aliquam vitae fringilla unda.</p>
-							</div>
-						</div>
-					</div>
-					<!-- end team member item -->
 				</div>
 			</div>
 		</section>
@@ -220,25 +102,13 @@
 				<div class="row">
 					<div class="col-lg-12 text-center">
 						<div class="section-title">
-							<h2>Contact Us</h2>
-							<p>If you have some Questions or need Help! Please Contact Us!<br>We make Cool and Clean Design for your Business</p>
+							<h2>Contato</h2>
+							<p>Entre em contato conosco por aqui.<br>Tentaremos responder o mais rápido possível.</p>
 						</div>
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-3">
-						<h3>Our Business Office</h3>
-						<p>3422 Street, Barcelona 432, Spain, New Building North, 15th Floor</p>
-						<p><i class="fa fa-phone"></i> +101 377 655 22125</p>
-						<p><i class="fa fa-envelope"></i> mail@yourcompany.com</p>
-					</div>
-					<div class="col-md-3">
-						<h3>Business Hours</h3>
-						<p><i class="fa fa-clock-o"></i> <span class="day">Weekdays:</span><span>9am to 8pm</span></p>
-						<p><i class="fa fa-clock-o"></i> <span class="day">Saturday:</span><span>9am to 2pm</span></p>
-						<p><i class="fa fa-clock-o"></i> <span class="day">Sunday:</span><span>Closed</span></p>
-					</div>
-					<div class="col-md-6">
+					<div class="col-md-12">
 						<form name="sentMessage" id="contactForm" novalidate="">
 							<div class="row">
 								<div class="col-md-6">
@@ -266,7 +136,7 @@
 							<div class="row">
 								<div class="col-lg-12 text-center">
 									<div id="success"></div>
-									<button type="submit" class="btn">Send Message</button>
+									<button type="submit" class="btn btn-success">Enviar</button>
 								</div>
 							</div>
 						</form>
