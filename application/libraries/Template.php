80391c7d23f729565ff3c45e77bc3b9ff3ac5497
Pedro Martins <pedrogmhead@gmail.com>
2019-12-30 16:18:41 -0300

    added template.php

---
+++ application/libraries/Template.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed')
+    class template {
+            function show($view, $data=array()){
+                $CI = & get_instance();
+                //load header
+                $CI->load->view('template/header', $data);
+                //load content
+                $CI->load->view($view, $data);
+                //load footer
+                $CI->load->view('template/footer', $data);
+                //scripts
+                $CI->load->view('template/scripts', $data);
+            }
+    }
