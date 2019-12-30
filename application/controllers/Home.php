a7e097ffb1212703e8cb771e1e86330c14509ef9
Pedro Martins <pedrogmhead@gmail.com>
2019-12-30 16:52:35 -0300

    changed links to base_url()

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$this->template->view('home');
+		$this->template->show('home');
 	}
 }
