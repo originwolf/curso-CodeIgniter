5fa995e14fc6bfec0c513ea20bd53aaa1670584f
Pedro Martins <pedrogmhead@gmail.com>
2020-01-05 10:23:44 -0300

    created util.js

---
+++ public/js/util.js
@@ -0,0 +1,20 @@
+const BASE_URL = "localhost/CodeIgniter"
+
+function clearErrors() {
+    $(".has-error").removeClass("has-error")
+    $(".help-block").html("")
+}
+
+function showErrors(error_list) {
+    clearErrors()
+
+    $.each(error_list, function(id, message) {
+        $(id).parent().parent().addClass("has-error")
+
+        $(id).parent().siblings(".help-block").html(message)
+    })
+}
+
+function loadingImg(message="") {
+    return "<i class='fa fa-circle-o-notch fa-spin'></i>&nbsp;" + message
+}
